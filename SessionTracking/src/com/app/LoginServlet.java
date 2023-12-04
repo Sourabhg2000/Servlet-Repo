d301d58a4cc55d2ff31102b20d61c9f82b761cd6
Sourabh Gaikwad <sourabhg9009@gmail.com>
2023-12-04 17:03:37 +0530

    Servlet Session Tracking

---
+++ SessionTracking/src/com/app/LoginServlet.java
@@ -0,0 +1,64 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.omg.CORBA.Request;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter pw =response.getWriter();
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
+		dispatcher.include(request, response);
+		String uname=request.getParameter("txtUserName");
+		String pwd=request.getParameter("txtPassWord");
+		if(uname.equals("admin")&& pwd.equals("admin123")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", uname);
+			session.setAttribute("pass", pwd);
+			pw.write("Successfully logged in");
+			}
+		else {
+			pw.write("invalid username or password");
+			request.getRequestDispatcher("login.html").include(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
